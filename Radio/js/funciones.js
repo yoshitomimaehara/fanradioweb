b8d7deaa9355addfd58bb8c27fb888af63fd419d
yoshitomimaehara <trombon2004@gmail.com>
2014-01-25 14:52:44 -0500

    Avance de Codigo
    Ya extrae datos de JSON,estan encapsulados en una funcion en JQuery
    Falta
    - comparar horas
    - crear estructura con los horarios y programas

--- Radio/js/funciones.js
+++ Radio/js/funciones.js
@@ -1,16 +1,21 @@
-	function getPrograma(num){
-		
-	
-	}
-	
-	function cargaImagenes(num){
-		$.getJSON('json/programas.json',function(data){
-			$('#image').value("src","img/" + data.programas[num].img);
-		});
-	}
-	
-	function getLocutor(num){
-			$.getJSON('json/programas.json',function(data){
-			$('#loc').append(data.programas[num].dj);
+function init_data(){
+      var num = ;
+      $.getJSON('json/programas.json',function(data){
+			  $('#loc').append(data.programas[num].dj);
+        $('#image').append('<img src='+"img/" + data.programas[num].img+'/>');
 			});
-	}
+}
+
+function getNumber(){
+    var ahora = new Date;
+	var horanow=ahora.getHours();
+	var minnow=ahora.getMinutes());
+	
+   return
+}
+
+
+
+
+
+
