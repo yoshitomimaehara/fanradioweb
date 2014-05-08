ff608c2573ca570ee4421c90868a4bcd9a26a2c2
yoshitomimaehara <trombon2004@gmail.com>
2014-05-08 13:23:59 -0500

    Avance de Codigo
    Ya permite mostrar:
    - Las Imagenes
    - El DJ
    - El nombre del programa
    
    Falta:
    - hacer bien la comparacion de hora
    - testear bien

--- Radio/js/dom.js
+++ Radio/js/dom.js
@@ -1,6 +1,6 @@
 $('document').ready(function(){
-		var cadena $.getJSON('json/programas.json',function(jd){
-			$('#loc').html('<p>'+jd.Fanradio.programas[1].locutor+'</p>');
+		var cadena= $.getJSON('json/programas.json',function(jd){
+			$('#loc').html('<p>'+jd.programas[1].dj+'</p>');
 		});
 })
 	
--- Radio/js/funciones.js
+++ Radio/js/funciones.js
@@ -1,21 +1,51 @@
-function init_data(){
-      var num = ;
-      $.getJSON('json/programas.json',function(data){
-			  $('#loc').append(data.programas[num].dj);
-        $('#image').append('<img src='+"img/" + data.programas[num].img+'/>');
-			});
-}
-
-function getNumber(){
+function init_data() {
     var ahora = new Date;
-	var horanow=ahora.getHours();
-	var minnow=ahora.getMinutes());
-	
-   return
+    var horanow = ahora.getHours();
+    //var minnow = ahora.getMinutes();
+    var dianow = ahora.getDay();
+    $.getJSON('json/programas.json', function (data) {
+        var cont = 0;
+        $.each(data.programas, function (i, item) {
+            cont++;
+        });
+        var i = 0;
+        var n;
+        var flag = false;
+        while (i < cont || flag == false) {
+            if (dianow == data.programas[i].dia) {
+                var k=0;
+                while (k < cont) {
+                    if ((horanow >= data.programas[k].horini || horanow <= data.programas[k].horfin) && dianow == data.programas[i].dia) {
+                        n = k;
+                        flag = true;
+                    }
+                    k++;
+                }
+            }
+            i++;
+        }
+        if (flag == false) {
+            n = 0;
+        }
+        console.log(n);
+        $('#loc').text(data.programas[n].dj);
+        $('#image').html('<img src=' + "img/" + data.programas[n].img + '/>');
+        $('#titulo').text(data.programas[n].nombre);
+    });
+    refrescar();
+}
+
+function limpiar() {
+    $('#loc').empty();
+    $('#image').empty();
+    $('#titulo').empty();
 }
 
 
-
-
+function refrescar() {
+    limpiar();
+    window.setInterval(5000);
+    init_data();
+}
 
 
